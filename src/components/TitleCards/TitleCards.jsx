9409fc74d1630f6874c9aa37a2c340bec25f3ebc
Ritanshu Juneja <ritanshujuneja@gmail.com>
2025-09-23 16:31:49 +0530

    login form

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -4,12 +4,12 @@ import cards_data from '../../assets/cards/Cards_data'
 
 
 
-const TitleCards = () => {
+const TitleCards = ({title, category}) => {
 const cardsRef = useRef();
 
   return (
     <div className='title-cards'>
-      <h2>Popular on Netflix</h2>
+      <h2>{title?title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
         {
           cards_data.map((card,index)=>{
